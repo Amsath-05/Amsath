398c7027f200f1f99565b11e822c33cf8af3f8c3
Amsath-05 <102526679+Amsath-05@users.noreply.github.com>
2022-05-04 16:30:35 -1200

    Add files via upload

---
+++ day3/factorial.java
@@ -0,0 +1,11 @@
+package week1.day3;
+
+public class factorial {
+	public static void main(String[] args) {
+		int i=1, fact=1;
+		for( i=1;i<=5;i++) {
+			fact=fact*i;
+	}
+	System.out.println(fact);
+	}
+}
