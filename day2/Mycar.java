6010d9f0ff072ea0395d15f38be586bcc38dd745
Amsath-05 <102526679+Amsath-05@users.noreply.github.com>
2022-05-11 12:14:33 +0530

    Add files via upload

---
+++ day2/Mycar.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class Mycar {
+	public static void main(String[] args) {
+		car c= new car();
+		c.applyAccelerator();
+		c.applyBreak();
+		c.applyGear();
+		c.switchOnAC();
+	}
+	
+
+
+}
