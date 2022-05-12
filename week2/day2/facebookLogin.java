c8fc30ba8b544415a261e3a1156712b0f9f9ec63
Amsath-05 <102526679+Amsath-05@users.noreply.github.com>
2022-05-12 10:57:15 +0530

    Add files via upload

--- week2/day2/facebookLogin.java
+++ week2/day2/facebookLogin.java
@@ -10,8 +10,8 @@ public static void main(String[] args) {
 	WebDriverManager.chromedriver().setup();
 	ChromeDriver driver= new ChromeDriver();
 	driver.get("https://www.facebook.com/");
-	driver.findElement(By.id("email")).sendKeys("8220571668");
-	driver.findElement(By.id("pass")).sendKeys("22061974");
+	driver.findElement(By.id("email")).sendKeys("9283993984");
+	driver.findElement(By.id("pass")).sendKeys("0029739203");
 	driver.findElement(By.name("login")).click();
 	
 }
