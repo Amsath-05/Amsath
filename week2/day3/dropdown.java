a0523ca36704ad899e6d85b1c05459980c1bc59b
Amsath-05 <102526679+Amsath-05@users.noreply.github.com>
2022-05-12 11:47:57 +0530

    Add files via upload

---
+++ week2/day3/dropdown.java
@@ -0,0 +1,26 @@
+package week2.day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class dropdown {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		WebElement findElement = driver.findElement(By.id("createLeadForm_dataSourceId"));
+		Select soc=new Select(findElement);
+		soc.selectByValue("LEAD_COLDCALL");
+	}
+
+}
