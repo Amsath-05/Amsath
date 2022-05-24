eb235c78c2d99d9240600a176e8c3c7bd0ed58a2
Amsath-05 <102526679+Amsath-05@users.noreply.github.com>
2022-05-24 11:44:14 +0530

    Add files via upload

--- week4/day1/SBI.java
+++ week4/day1/SBI.java
@@ -2,7 +2,7 @@ package week4.day1;
 
 public abstract class SBI  {
 public void ITLoan() {
-	
+	System.out.println("ITLoan");
 }
 public abstract void bankBalance();
 public abstract void maximamLoanAmount();
